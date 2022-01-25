ad9385f09f5e6c2e0ff9c0cf315b15a7217f6660
Martín <magadiflo@gmail.com>
2022-01-25 12:05:50 -0500

    Configurar CORS y body de las peticiones

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,9 @@
 const { response } = require('express');
 
 const crearUsuario = (req, res = response) => {
+
+    const {email, name, password} = req.body;
+
     return res.json({
         ok: true,
         msg: 'Crear usuario /new',
@@ -8,6 +11,9 @@ const crearUsuario = (req, res = response) => {
 }
 
 const loginUsuario = (req, res) => {
+
+    const {email, password} = req.body;
+
     return res.json({
         ok: true,
         msg: 'Login de usuario /',
