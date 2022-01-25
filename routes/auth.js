1f50f6117f4f5943f3d96afc812a0bf6a7d28f4d
Martín <magadiflo@gmail.com>
2022-01-25 11:57:40 -0500

    Separar el controlador de la ruta

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,15 @@
 const { Router } = require('express');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
 const router = Router();
 
 //Crear un nuevo usuario
-router.post('/new', (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario /new',
-    });
-});
+router.post('/new', crearUsuario);
 
 //Login de usuario
-router.post('/', (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Login de usuario /',
-    });
-});
+router.post('/', loginUsuario);
 
 //Validar y revalidar token
-router.get('/renew', (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Renew',
-    });
-});
+router.get('/renew', revalidarToken);
 
 module.exports = router;
